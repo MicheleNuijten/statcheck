170b4c130573a2c81221a60b0cd5555133f03a52
Michele Nuijten <michele.nuijten@gmail.com>
2012-07-25 11:58:57 +0200

    nu met exacte p-waardes aangegeven

--- R/plotmethod.R
+++ R/plotmethod.R
@@ -1,7 +1,7 @@
 plot.statcheck <- function(x,...) {
   
-  reported <- x$Reported.P.Value[x$Reported.Comparison=="="]
-  computed <- x$Computed[x$Reported.Comparison=="="]
+  reported <- x$Reported.P.Value
+  computed <- x$Computed
   
   # scatterplot of reported and recalculated p values
   plot.default(reported,computed,
@@ -10,15 +10,19 @@ plot.statcheck <- function(x,...) {
                xlim=c(0,1),ylim=c(0,1),
                pch=20)
   
-  
   # red dot for gross error (non-sig reported as sig and vice versa)
-  points(reported[which(reported>.05 & computed<.05)],
-         computed[which(reported>.05 & computed<.05)],
+  points(reported[reported>.05 & computed<.05],
+         computed[reported>.05 & computed<.05],
          pch=20,col="red")
   
-  points(reported[which(reported<.05 & computed>.05)],
-         computed[which(reported<.05 & computed>.05)],
+  points(reported[reported<.05 & computed>.05],
+         computed[reported<.05 & computed>.05],
          pch=20,col="red")
+  
+  # triangles for exact p values
+  points(x$Reported.P.Value[x$Reported.Comparison=="="],
+         x$Computed[x$Reported.Comparison=="="],
+         pch=5)
   
   
   # general layout of figure:
@@ -36,4 +40,8 @@ plot.statcheck <- function(x,...) {
   
   text(.5,0,"sig reported as non-sig",cex=.7)
   
+  legend("topleft",
+         pch=c(20,5),
+         col=c("red","black"),
+         legend=c("gross error","exact"))
 }
