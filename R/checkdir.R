86813888f065865f1f208f76c9bd401eda988ca2
Sacha Epskamp <sacha.epskamp@gmail.com>
2012-07-28 13:01:14 +0200

    Fixed bug requiring both html and pdfs for checkdir()

--- R/checkdir.R
+++ R/checkdir.R
@@ -4,8 +4,8 @@
 checkdir <- function(dir,...)
 {
   if (missing(dir)) dir <- tk_choose.dir()
-  pdfres <- checkPDFdir(dir,...)
-  htmlres <- checkHTMLdir(dir,...)
+  if (any(grepl("\\.pdf",list.files(dir)))) pdfres <- checkPDFdir(dir,...)
+  if (any(grepl("\\.html",list.files(dir)))) htmlres <- checkHTMLdir(dir,...)
   Res <- rbind(pdfres,htmlres)
   class(Res) <- c("statcheck","data.frame")
   return(Res)
