73653c715b19c09cc12424351259b6cff180d3ff
Sacha Epskamp <sacha.epskamp@gmail.com>
2012-08-17 14:33:11 +0200

    Added check PDF or HTML

--- R/checkdir.R
+++ R/checkdir.R
@@ -3,12 +3,17 @@
 ## Function to check directory of PDFs:
 checkdir <- function(dir,...)
 {
+  pdfs <- any(grepl("\\.pdf",list.files(dir)))
+  htmls <- any(grepl("\\.html",list.files(dir)))
+  
   if (missing(dir)) dir <- tk_choose.dir()
-#   if (any(grepl("\\.pdf",list.files(dir)))) 
-    pdfres <- checkPDFdir(dir,...)
-#   if (any(grepl("\\.html",list.files(dir)))) 
-    htmlres <- checkHTMLdir(dir,...)
-  Res <- rbind(pdfres,htmlres)
+   if (pdfs) pdfres <- checkPDFdir(dir,...)
+   if (htmls) htmlres <- checkHTMLdir(dir,...)
+  
+  if (pdfs & htmls)
+  {
+    Res <- rbind(pdfres,htmlres)
+  } else if (pdfs & !htmls) Res <- pdfres else if (!pdfs & htmls) Res <- htmlres else stop("No PDF or HTML found")
   class(Res) <- c("statcheck","data.frame")
   return(Res)
 }
