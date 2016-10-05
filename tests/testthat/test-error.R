e3b9481b9ccadbba72f4801f1d2061b9d1d8c11b
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-10-05 12:41:47 -0500

    remove testthat:: lines

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -4,8 +4,8 @@ test_that("Detecting a p-value error from a t-test", {
   txt <- "The effect was very significant (t(48) = 1.02, p < .05)"
   result <- statcheck(txt)
 
-  testthat::expect_equal(as.character(result$Statistic), "t")
-  testthat::expect_true(result$Error)
-  testthat::expect_true(result$DecisionError)
+  expect_equal(as.character(result$Statistic), "t")
+  expect_true(result$Error)
+  expect_true(result$DecisionError)
 })
 
