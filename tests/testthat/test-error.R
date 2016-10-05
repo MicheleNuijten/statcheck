14377dcd6029b4388da4d3016f0c6f8cc0ec9499
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-10-05 10:25:26 -0500

    add unit tests for t-tests

---
+++ tests/testthat/test-error.R
@@ -0,0 +1,10 @@
+context("Catching errors")
+
+test_that("Detecting a p-value error from a t-test", {
+  txt <- "The effect was very significant (t(48) = 1.02, p < .05)"
+  result <- statcheck(txt)
+
+  testthat::expect_equal(as.character(result$Statistic), "t")
+  testthat::expect_true(result$Error)
+  testthat::expect_true(result$DecisionError)
+})
