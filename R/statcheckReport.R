e9f833aa52f83b91219d7355ed2c119b6c9da29c
M.B. Nuijten <mnuijten@campus.uvt.nl>
2018-05-28 16:36:55 +0200

    Reset working directory after running statcheckReport()

--- R/statcheckReport.R
+++ R/statcheckReport.R
@@ -2,8 +2,10 @@ statcheckReport <-
   function(statcheckOutput,
            outputFileName,
            outputDir) {
+    
     # set working directory to output file in statcheck package library
-    setwd(system.file("rmd", package = "statcheck"))
+    currentWD <- getwd()
+    setwd(system.file("inst/rmd", package = "statcheck"))
     
     # temporarily save statcheck output as RData in the selected working directory
     save(statcheckOutput, file = "statcheckOutput.RData")
@@ -17,10 +19,11 @@ statcheckReport <-
     curDir <- system.file("rmd", package = "statcheck")
     file.rename(
       from = paste(curDir, "statcheckReport_template.html", sep = "/"),
-      to = paste(outputDir, "/", outputFileName, ".html", sep =
-                   "")
+      to = paste(outputDir, "/", outputFileName, ".html", sep = "")
     )
     
     # remove .RData file from package library folder
     file.remove(paste(curDir, "statcheckOutput.RData", sep = "/"))
+    
+    setwd(currentWD)
   }
