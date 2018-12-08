2e405bf91b1823c63e44541617b3d20cee3bbb6d
Hugo Gruson <hugo.gruson@protonmail.com>
2018-12-08 09:21:36 +0100

    Restore original wd on exit This is more robust. With the old version, a crash during the function execution would leave the modified wd

--- R/statcheckReport.R
+++ R/statcheckReport.R
@@ -2,28 +2,28 @@ statcheckReport <-
   function(statcheckOutput,
            outputFileName,
            outputDir) {
-    
+
     # set working directory to output file in statcheck package library
     currentWD <- getwd()
+    on.exit(setwd(currentWD))
     setwd(system.file("inst/rmd", package = "statcheck"))
-    
+
     # temporarily save statcheck output as RData in the selected working directory
     save(statcheckOutput, file = "statcheckOutput.RData")
-    
+
     # run the markdown/knitr script
     statcheckReport_template <-
       system.file("rmd/statcheckReport_template.Rmd", package = "statcheck")
     render(statcheckReport_template)
-    
+
     # save/move the file in/to the specified output directory
     curDir <- system.file("rmd", package = "statcheck")
     file.rename(
       from = paste(curDir, "statcheckReport_template.html", sep = "/"),
       to = paste(outputDir, "/", outputFileName, ".html", sep = "")
     )
-    
+
     # remove .RData file from package library folder
     file.remove(paste(curDir, "statcheckOutput.RData", sep = "/"))
-    
-    setwd(currentWD)
+
   }
