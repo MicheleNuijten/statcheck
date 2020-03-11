2412d620ffe270a9bdf9881d6065047643b3d7ee
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-11 10:11:03 +0100

    suppress statcheck's progress bars during testing

--- tests/testthat/test_extract_t.R
+++ tests/testthat/test_extract_t.R
@@ -8,7 +8,7 @@ test_that("standard t-tests are retrieved from text", {
   txt2 <- "The effect was very significant, t(28) = 2.20, p = .03."
   txt3 <- "Both effects were very significant, t(28) = 2.20, p = .03, t(28) = 1.23, p = .04."
   
-  result <- statcheck(c(txt1, txt2, txt3))
+  ddpcr::quiet(result <- statcheck(c(txt1, txt2, txt3)))
   
   expect_equal(nrow(result), 4)
 })
@@ -18,7 +18,7 @@ test_that("t-tests with different spacing are retrieved from text", {
   txt1 <- " t ( 28 ) = 2.20 , p = .03"
   txt2 <- "t(28)=2.20,p=.03"
   
-  result <- statcheck(c(txt1, txt2))
+  ddpcr::quiet(result <- statcheck(c(txt1, txt2)))
   
   expect_equal(nrow(result), 2)
 })
@@ -30,7 +30,7 @@ test_that("variations in the t-statistic are retrieved from text", {
   txt3 <- "t(28) < 2.20, p = .03"
   txt4 <- "t(28) > 2.20, p = .03"
   
-  result <- statcheck(c(txt1, txt2, txt3, txt4))
+  ddpcr::quiet(result <- statcheck(c(txt1, txt2, txt3, txt4)))
   
   expect_equal(nrow(result), 4)
 })
@@ -43,7 +43,7 @@ test_that("variations in the p-value are retrieved from text", {
   txt4 <- "t(28) = 2.20, ns"
   txt5 <- "t(28) = 2.20, p = .5e-3"
   
-  result <- statcheck(c(txt1, txt2, txt3, txt4, txt5))
+  ddpcr::quiet(result <- statcheck(c(txt1, txt2, txt3, txt4, txt5)))
   
   expect_equal(nrow(result), 5)
 })
@@ -52,7 +52,7 @@ test_that("variations in the p-value are retrieved from text", {
 test_that("corrected degrees of freedom in t-tests are retrieved from text", {
   txt1 <- "t(28.1) = 2.20, p = .03"
   
-  result <- statcheck(txt1)
+  ddpcr::quiet(result <- statcheck(txt1))
   
   expect_equal(nrow(result), 1)
 })
@@ -64,20 +64,20 @@ test_that("incorrect punctuation in t-tests are not retrieved from text", {
   txt1 <- "t(28) = 2.20; p = .03"
   txt2 <- "t[28] = 2.20, p = .03"
   
-  expect_output(statcheck(c(txt1, txt2)), "did not find any results")
+  ddpcr::quiet(expect_output(statcheck(c(txt1, txt2)), "did not find any results"))
 })
 
 # not a t-test
 test_that("the word 'test' is not recognized as a t-test in text", {
   txt1 <- "test(28) = 2.20, p = .03"  
   
-  expect_output(statcheck(txt1), "did not find any results")
+  ddpcr::quiet(expect_output(statcheck(txt1), "did not find any results"))
 })
 
 # not a p-value
 test_that("tests with 'p-values' larger than 1 are not retrieved from text", {
   txt1 <- "t(28) = 2.20, p = 1.03"
   
-  expect_output(statcheck(txt1), "did not find any results")
+  ddpcr::quiet(expect_output(statcheck(txt1), "did not find any results"))
 })
 
