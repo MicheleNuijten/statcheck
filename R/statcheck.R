3e5c89237e1c919ef20696031344d8fdc0c8be06
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-16 16:13:34 +0100

    replace "raw" code to check for one-tailed tests with the new function

--- R/statcheck.R
+++ R/statcheck.R
@@ -72,6 +72,7 @@ statcheck <- function(texts,
   ###---------------------------------------------------------------------
   
   if (nrow(Res) > 0) {
+    
     # if indicated, count all tests as onesided
     if (OneTailedTests == TRUE) {
       Res$Computed <- Res$Computed / 2
@@ -83,7 +84,11 @@ statcheck <- function(texts,
     Res$DecisionError <-  DecisionErrorTest(Res, alpha = alpha, 
                                             pEqualAlphaSig = pEqualAlphaSig)
     
-    ###---------------------------------------------------------------------
+    ### print messages ----------------------------------------------------
+   
+    # check if there could be one-sided tests in the data set
+    Res <- check_presence_1tail(Res,
+                                messages = messages) 
     
     # check if there would also be a decision error if alpha=.01 or .1
     check_alpha_levels(Res, 
@@ -91,41 +96,6 @@ statcheck <- function(texts,
     
     ###---------------------------------------------------------------------
     
-    if (OneTailedTests == FALSE) {
-      # check if there could be one-sided tests in the data set
-      
-      computed <- Res$Computed
-      comparison <- Res$Reported.Comparison
-      reported <- Res$Reported.P.Value
-      raw <- Res$Raw
-      onetail <- computed / 2
-      
-      OneTail <- ifelse(
-        Res$Error == TRUE &
-          (
-            grepl("=", comparison) & round(reported, 2) == round(onetail, 2)
-          )
-        |
-          (
-            grepl("<", comparison) &
-              onetail < reported & computed > reported
-          ),
-        TRUE,
-        FALSE
-      )
-      Res$OneTail <- OneTail
-      
-      if (messages == TRUE & 
-          any(OneTail[!is.na(OneTail)] == TRUE &
-              OneTailedTxt[!is.na(OneTailedTxt)] == FALSE)) {
-        message(
-          "\n Check for one tailed tests. \n \n Some of the p value incongruencies might in fact be one tailed tests. It is recommended to check this in the actual paper or text. Check if the p values would also be incongruent if the test is indeed one sided by running statcheck again with 'OneTailedTests' set to TRUE. To see which Sources probably contain a one tailed test, try unique(x$Source[x$OneTail]) (where x is the statcheck output). \n "
-        )
-      }
-      
-    }
-    
-    ###---------------------------------------------------------------------
     
     # count errors as correct if they'd be correct one-sided
     # and there was a mention of 'one-sided','one-tailed', or 'directional' in the text
