c50f1850e7183984b456a482426bb1186b16198e
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-16 15:58:37 +0100

    get info from Res on whether the string "one-tailed" etc is in text

--- R/check_presence_1tail.R
+++ R/check_presence_1tail.R
@@ -5,6 +5,7 @@ check_presence_1tail <- function(Res, messages, OneTailedTxt) {
   reported <- Res$Reported.P.Value
   raw <- Res$Raw
   onetail <- computed / 2
+  onetailtxt <- Res$OneTailedInTxt
   
   OneTail <- ifelse(
     Res$Error == TRUE & (grepl("=", comparison) 
@@ -17,7 +18,7 @@ check_presence_1tail <- function(Res, messages, OneTailedTxt) {
   
   if (messages == TRUE & 
       any(OneTail[!is.na(OneTail)] == TRUE &
-          OneTailedTxt[!is.na(OneTailedTxt)] == FALSE)) {
+          onetailtxt[!is.na(onetailtxt)] == FALSE)) {
     message(
       "\n Check for one tailed tests. \n \n Some of the p value incongruencies might in fact be one tailed tests. It is recommended to check this in the actual paper or text. Check if the p values would also be incongruent if the test is indeed one sided by running statcheck again with 'OneTailedTests' set to TRUE. To see which Sources probably contain a one tailed test, try unique(x$Source[x$OneTail]) (where x is the statcheck output). \n "
     )
