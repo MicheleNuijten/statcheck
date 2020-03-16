e70a2008dfb090b195044b69c0bdfd32df08d536
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-16 11:36:29 +0100

    move function to check correct rounding to its own script

--- R/statcheck.R
+++ R/statcheck.R
@@ -183,82 +183,10 @@ statcheck <- function(texts,
     # e.g. t=2.3 could be 2.25 to 2.34999999... with its range of p values
     correct_round <- numeric()
     
-    lower <- Res$Value - (.5 / 10 ^ Res$testdec)
-    upper <- Res$Value + (.5 / 10 ^ Res$testdec)
-    
     for (i in seq_len(nrow(Res))) {
-      if (Res[i, ]$Statistic == "F") {
-        upP <- pf(lower[i], Res[i, ]$df1, Res[i, ]$df2, lower.tail = FALSE)
-        lowP  <-
-          pf(upper[i], Res[i, ]$df1, Res[i, ]$df2, lower.tail = FALSE)
-        
-      } else if (Res[i, ]$Statistic == "t") {
-        if (lower[i] < 0) {
-          lowP <- pt(lower[i], Res[i, ]$df2) * 2
-          upP  <- pt(upper[i], Res[i, ]$df2) * 2
-        } else{
-          upP <- pt(-1 * lower[i], Res[i, ]$df2) * 2
-          lowP  <- pt(-1 * upper[i], Res[i, ]$df2) * 2
-        }
-        
-      } else if (Res[i, ]$Statistic == "Chi2" |
-                 Res[i, ]$Statistic == "Q" |
-                 Res[i, ]$Statistic == "Qw" | 
-                 Res[i, ]$Statistic == "Qb") {
-        upP <- pchisq(lower[i], Res[i, ]$df1, lower.tail = FALSE)
-        lowP  <- pchisq(upper[i], Res[i, ]$df1, lower.tail = FALSE)
-        
-      } else if (Res[i, ]$Statistic == "r") {
-        if (lower[i] < 0) {
-          lowP <- pmin(pt(r2t(lower[i], Res[i, ]$df2), Res[i, ]$df2) * 2, 1)
-          upP  <-
-            pmin(pt(r2t(upper[i], Res[i, ]$df2), Res[i, ]$df2) * 2, 1)
-        } else {
-          upP <- pmin(pt(-1 * r2t(lower[i], Res[i, ]$df2), Res[i, ]$df2) * 2, 1)
-          lowP  <-
-            pmin(pt(-1 * r2t(upper[i], Res[i, ]$df2), Res[i, ]$df2) * 2, 1)
-        }
-        
-      } else if (Res[i, ]$Statistic == "Z" |
-                 Res[i, ]$Statistic == "z") {
-        if (lower[i] < 0) {
-          lowP <- pnorm(abs(lower[i]), lower.tail = FALSE) * 2
-          upP  <- pnorm(abs(upper[i]), lower.tail = FALSE) * 2
-        } else {
-          upP <- pnorm(lower[i], lower.tail = FALSE) * 2
-          lowP  <- pnorm(upper[i], lower.tail = FALSE) * 2
-        }
-        
-      }
       
-      if (OneTailedTests == TRUE) {
-        upP <- upP / 2
-        lowP <- lowP / 2
-      }
-      
-      if (Res[i, "Reported.Comparison"] == "=") {
-        correct_round[i] <-
-          ifelse(
-            Res[i, ]$Error == TRUE &
-              Res$Reported.P.Value[i] >= round(lowP, Res$dec[i]) &
-              Res$Reported.P.Value[i] <= round(upP, Res$dec[i]),
-            TRUE,
-            FALSE
-          )
-      }
-      
-      if (Res[i, "Reported.Comparison"] == "<") {
-        correct_round[i] <-
-          ifelse(Res[i, ]$Error == TRUE &
-                   Res$Reported.P.Value[i] > lowP, TRUE, FALSE)
-      }
-      
-      if (Res[i, "Reported.Comparison"] == ">") {
-        correct_round[i] <-
-          ifelse(Res[i, ]$Error == TRUE &
-                   Res$Reported.P.Value[i] < upP, TRUE, FALSE)
-      }
-      
+      correct_round[i] <- check_correct_rounding(Res[i, ], 
+                                                 OneTailedTests = OneTailedTests)
       
     }
     
