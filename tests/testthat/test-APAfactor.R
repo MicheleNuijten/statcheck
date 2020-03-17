5ee9f3e13e3831f84af11a8f97d3d8952013108e
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-17 13:24:08 +0100

    extend unit test for APA factor

--- tests/testthat/test-APAfactor.R
+++ tests/testthat/test-APAfactor.R
@@ -2,11 +2,15 @@ context("Calculate the APA factor")
 
 test_that("correct APA factor is calculated", {
   
-  txt <- "This text has 50% of its stats in APA style: t(28) = 2.20, p < .05, some other p = .035"
-  txt2 <- "This text also has 50% of its stats in APA style: t(28) = 2.20, p < .05, some other p = .035, t(29) = 2.20, p < .05, some other p = .045"
+  txt1 <- "This text has 50% of its stats in APA style: t(28) = 2.20, p < .05, some other p = .035."
+  txt2 <- "This text has 100% of its stats in APA style: t(28) = 2.20, p < .05."
   
-  result <- statcheck(txt, messages = FALSE)
+  result1 <- statcheck(txt1, messages = FALSE)
+  result2 <- statcheck(txt2, messages = FALSE)
+  result12 <- statcheck(c(txt1, txt2), messages = FALSE)
   
-  expect_equal(result$APAfactor, .5)
+  expect_equal(result1$APAfactor, .5)
+  expect_equal(result2$APAfactor, 1)
+  expect_equal(result12$APAfactor, c(.5, 1))
     
 })
