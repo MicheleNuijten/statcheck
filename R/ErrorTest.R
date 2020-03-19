d78174595b2c7c30df7bdf4bf093757d92dd2c59
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-19 15:47:39 +0100

    rewrote error test

--- R/ErrorTest.R
+++ R/ErrorTest.R
@@ -1,77 +1,55 @@
-ErrorTest <- function(x, alpha, ...) {
+ErrorTest <- function(reported_p, computed_p, 
+                      test_type, test_stat,
+                      df1, df2,
+                      p_comparison, test_comparison, 
+                      p_dec, test_dec, alpha, ...) {
   
-  computed <- as.vector(x$Computed)
-  comparison <- as.vector(x$Reported.Comparison)
-  reported <-  as.vector(x$Reported.P.Value)
-  testcomp <-  as.vector(x$Test.Comparison)
+  # replace 'ns' for > alpha -----------------------------------------------
   
-  # replace 'ns' for > alpha
-  reported[comparison == "ns"] <- alpha
-  comparison[comparison == "ns"] <- ">"
+  reported_p[p_comparison == "ns"] <- alpha
+  p_comparison[p_comparison == "ns"] <- ">"
   
-  Match <- paste(computed, comparison, reported)
+  # compute upper and lower bound for p-value ------------------------------
   
-  #-----------------------------------------------
+  low_stat <- test_stat - (.5 / 10 ^ test_dec)
+  up_stat <- test_stat + (.5 / 10 ^ test_dec)
   
-  # select inexactly reported p values (p<../p>..)
-  InExTests <- grepl("<|>", Match)
+  low_p <- compute_p(test_type, up_stat, df1, df2)
+  up_p <- compute_p(test_type, low_stat, df1, df2)
   
-  # evaluate errors when test statistics are reported exactly (t()=.../F(,)=...)
-  if (any(InExTests)) {
-    InExTests[InExTests] <-
-      sapply(Match[InExTests], function(m)
-        ! eval(parse(text = m)))
+  # check errors for exact test statistics ---------------------------------
+  
+  if(test_comparison == "="){
+    
+    if(p_comparison == "="){
+      error <- reported_p > up_p & reported_p < low_p
+    } else if(p_comparison == "<"){
+      error <- reported_p < computed_p
+    } else if(p_comparison == ">"){
+      error <- reported_p > computed_p
+    }
+    
+  } else if(test_comparison == "<"){
+    
+    if(p_comparison == "="){
+      error <- reported_p < up_p
+    } else if(p_comparison == "<"){
+      error <- reported_p < up_p
+    } else if(p_comparison == ">"){
+      error <- FALSE
+    }
+    
+  } else if(test_comparison == ">"){
+    
+    if(p_comparison == "="){
+      error <- reported_p > low_p
+    } else if(p_comparison == "<"){
+      error <- FALSE
+    } else if(p_comparison == ">"){
+      error <- reported_p > low_p
+    }
+    
   }
   
-  # evaluate errors when test statistics are reported inexactly (t()</>.../F(,)</>...)
-  smallsmall <- testcomp == "<" & comparison == "<"
-  smallgreat <- testcomp == "<" & comparison == ">"
-  greatsmall <- testcomp == ">" & comparison == "<"
-  greatgreat <- testcomp == ">" & comparison == ">"
-  
-  if (any(smallsmall)) {
-    InExTests[smallsmall] <-
-      round(computed[smallsmall], x$dec[smallsmall]) <= round(reported[smallsmall], x$dec[smallsmall])
-  }
-  
-  if (any(greatgreat)) {
-    InExTests[greatgreat] <-
-      round(computed[greatgreat], x$dec[greatgreat]) >= round(reported[greatgreat], x$dec[greatgreat])
-  }
-  
-  # these combinations of < & > are logically always correct
-  InExTests[smallgreat] <- FALSE
-  InExTests[greatsmall] <- FALSE
-  
-  #-----------------------------------------------
-  
-  # select exactly reported p values (p=..)
-  ExTests <- comparison == "="
-  
-  # evaluate errors when test statistics are reported exactly (t()=.../F(,)=...)
-  if (any(ExTests)) {
-    ExTests[ExTests] <-
-      !(round(computed[ExTests], x$dec[ExTests]) == round(reported[ExTests], x$dec[ExTests]))
-  }
-  
-  # evaluate errors when test statistics are reported inexactly (t()</>.../F(,)</>...)
-  smallequal <- x$Test.Comparison == "<" & comparison == "="
-  greatequal <- x$Test.Comparison == ">" & comparison == "="
-  
-  if (any(smallequal)) {
-    ExTests[smallequal] <-
-      round(computed[smallequal], x$dec[smallequal]) >= round(reported[smallequal], x$dec[smallequal])
-  }
-  
-  if (any(greatequal)) {
-    ExTests[greatequal] <-
-      round(computed[greatequal], x$dec[greatequal]) <= round(reported[greatequal], x$dec[greatequal])
-  }
-  
-  #-----------------------------------------------
-  
-  # a result is an error if InExactError and/or ExactError are TRUE
-  Error <- !(InExTests == FALSE & ExTests == FALSE)
-  
-  return(Error)
+  return(error)
 }
