43c0f0a1cc95c3545b1c7d84826e16051f769de9
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-20 11:21:07 +0100

    calculate upper & lower bound of p-value outside of error test

--- R/ErrorTest.R
+++ R/ErrorTest.R
@@ -1,5 +1,5 @@
-ErrorTest <- function(reported_p, computed_p, 
-                      test_type, test_stat,
+ErrorTest <- function(reported_p, test_type, test_stat,
+                      computed_p, low_p, up_p,
                       df1, df2,
                       p_comparison, test_comparison, 
                       p_dec, test_dec, alpha) {
@@ -8,14 +8,6 @@ ErrorTest <- function(reported_p, computed_p,
   
   reported_p[p_comparison == "ns"] <- alpha
   p_comparison[p_comparison == "ns"] <- ">"
-  
-  # compute upper and lower bound for p-value ------------------------------
-  
-  low_stat <- test_stat - (.5 / 10 ^ test_dec)
-  up_stat <- test_stat + (.5 / 10 ^ test_dec)
-  
-  low_p <- compute_p(test_type, up_stat, df1, df2)
-  up_p <- compute_p(test_type, low_stat, df1, df2)
   
   # check errors for exact test statistics ---------------------------------
   
