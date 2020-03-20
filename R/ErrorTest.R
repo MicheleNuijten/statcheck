653d1f2b0265e34b62cdb64d8fdd9c7e9b9a6f5c
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-20 09:52:49 +0100

    * add return statements throughout code to make code more efficient * fix typos

--- R/ErrorTest.R
+++ R/ErrorTest.R
@@ -2,7 +2,7 @@ ErrorTest <- function(reported_p, computed_p,
                       test_type, test_stat,
                       df1, df2,
                       p_comparison, test_comparison, 
-                      p_dec, test_dec, alpha, ...) {
+                      p_dec, test_dec, alpha) {
   
   # replace 'ns' for > alpha -----------------------------------------------
   
@@ -22,34 +22,61 @@ ErrorTest <- function(reported_p, computed_p,
   if(test_comparison == "="){
     
     if(p_comparison == "="){
-      error <- reported_p > up_p & reported_p < low_p
+      
+      error <- reported_p > up_p | reported_p < low_p
+      return(error)
+      
     } else if(p_comparison == "<"){
+      
       error <- reported_p < computed_p
+      return(error)
+      
     } else if(p_comparison == ">"){
+      
       error <- reported_p > computed_p
+      return(error)
+      
     }
     
   } else if(test_comparison == "<"){
     
     if(p_comparison == "="){
       error <- reported_p < up_p
+      return(error)
+      
     } else if(p_comparison == "<"){
+    
       error <- reported_p < up_p
-    } else if(p_comparison == ">"){
+      return(error)
+    } 
+    else if(p_comparison == ">"){
+      
       error <- FALSE
+      return(error)
+      
     }
     
   } else if(test_comparison == ">"){
     
     if(p_comparison == "="){
+      
       error <- reported_p > low_p
+      return(error)
+      
     } else if(p_comparison == "<"){
+      
       error <- FALSE
+      return(error)
+      
     } else if(p_comparison == ">"){
+      
       error <- reported_p > low_p
+      return(error)
+      
     }
     
   }
   
-  return(error)
+  return(NA)
+  
 }
