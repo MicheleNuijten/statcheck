06ef4fa2906a7efcfc9873ff0cc044eb2223fd08
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-20 13:21:04 +0100

    fix bug in error test for t = ... p </> ...

--- R/ErrorTest.R
+++ R/ErrorTest.R
@@ -1,5 +1,5 @@
 ErrorTest <- function(reported_p, test_type, test_stat,
-                      computed_p, low_p, up_p,
+                      low_p, up_p,
                       df1, df2,
                       p_comparison, test_comparison, 
                       p_dec, test_dec, alpha) {
@@ -20,12 +20,12 @@ ErrorTest <- function(reported_p, test_type, test_stat,
       
     } else if(p_comparison == "<"){
       
-      error <- reported_p < computed_p
+      error <- reported_p < low_p
       return(error)
       
     } else if(p_comparison == ">"){
       
-      error <- reported_p > computed_p
+      error <- reported_p > up_p
       return(error)
       
     }
