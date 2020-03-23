735510f32765d8042d67140507dff78c191a1a28
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-03-23 14:16:41 +0100

    add unit tests for classifying decision errors in t = ... , p = ...

--- tests/testthat/test-decisionerror.R
+++ tests/testthat/test-decisionerror.R
@@ -77,14 +77,42 @@ test_that("decision errors are correctly classified when argument p = alpha is c
 })
 
 
+# test classifications of (in)exact test statistcs and (in)exact p-values ----
 
-
-
-
-
-
-
-
+# test statistics exactly reported & p-value exactly reported
+test_that("cases where t = ..., p = ... are correctly classified", {
+  
+  # assume alpha = .05
+  # calculate which t-values correspond to p-values of .04, .05, and .06
+  t.04 <- qt(.04/2, 28, lower.tail = FALSE)
+  t.05 <- qt(.05/2, 28, lower.tail = FALSE)
+  t.06 <- qt(.06/2, 28, lower.tail = FALSE)
+  
+  txt1 <- paste0("t(28) = ", t.04, ", p = .04")
+  txt2 <- paste0("t(28) = ", t.05 + .0001, ", p = .04") # approach computed p == .05
+  txt3 <- paste0("t(28) = ", t.06, ", p = .04")
+  
+  txt4 <- paste0("t(28) = ", t.04, ", p = .05")
+  txt5 <- paste0("t(28) = ", t.05, ", p = .05")
+  txt6 <- paste0("t(28) = ", t.06, ", p = .05")
+  
+  txt7 <- paste0("t(28) = ", t.04, ", p = .06")
+  txt8 <- paste0("t(28) = ", t.05 + .0001, ", p = .06") # approach computed p == .05
+  txt9 <- paste0("t(28) = ", t.06, ", p = .06")
+  
+  expect_false(statcheck(txt1, messages = FALSE)$DecisionError)
+  expect_false(statcheck(txt2, messages = FALSE)$DecisionError)
+  expect_true(statcheck(txt3, messages = FALSE)$DecisionError)
+  
+  expect_false(statcheck(txt4, messages = FALSE)$DecisionError)
+  expect_false(statcheck(txt5, messages = FALSE)$DecisionError)
+  expect_true(statcheck(txt6, messages = FALSE)$DecisionError)
+  
+  expect_true(statcheck(txt7, messages = FALSE)$DecisionError)
+  expect_true(statcheck(txt8, messages = FALSE)$DecisionError)
+  expect_false(statcheck(txt9, messages = FALSE)$DecisionError)
+  
+})
 
 
 
