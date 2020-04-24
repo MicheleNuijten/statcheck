cacd9d0963bc1a4aa1f636f89e9b9e7d50e4a6ab
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-04-24 10:30:48 +0200

    create flexible variable names in a separate script

---
+++ R/constants.R
@@ -0,0 +1,23 @@
+# This file contains constants. These are mainly fixed variable names used
+# throughout the package
+# The logic of having a "constants" file, is that it now becomes straightforward
+# to change a variable name, without having to go through all R scripts
+
+# variables in final statcheck data frame
+VAR_SOURCE <- "source"
+VAR_TYPE <- "test_type"
+VAR_DF1 <- "df1"
+VAR_DF2 <- "df2"
+VAR_TEST_COMPARISON <- "test_comp"
+VAR_TEST_VALUE <- "test_value"
+VAR_P_COMPARISON <- "p_comp"
+VAR_REPORTED_P <- "reported_p"
+VAR_COMPUTED_P <- "computed_p"
+VAR_RAW <- "raw"
+VAR_ERROR <- "error"
+VAR_DEC_ERROR <- "decision_error"
+VAR_1TAILTXT <- "one_tailed_in_txt"
+VAR_APAFACTOR <- "apa_factor"
+
+# additional variables to return
+VAR_P_DEC <- "p_decimals"
