c861b20ec9611c0814ef0eb6bff38ab6b1ca05a5
MicheleNuijten <m.b.nuijten@uvt.nl>
2020-04-29 10:38:09 +0200

    update style

--- R/file-to-txt.R
+++ R/file-to-txt.R
@@ -34,18 +34,21 @@ getHTML <- function(x){
 }
 
 # PDF TO TXT -------------------------------------------------------------------
-getPDF <- function(x)
-{
+getPDF <- function(x){
+  
   txtfiles <- character(length(x))
-  for (i in 1:length(x))
-  {
+  for (i in 1:length(x)){
+    
     system(paste('pdftotext -q -enc "ASCII7" "', x[i], '"', sep = ""))
     if (file.exists(gsub("\\.pdf$", "\\.txt", x[i]))) {
       fileName <- gsub("\\.pdf$", "\\.txt", x[i])
       txtfiles[i] <- readChar(fileName, file.info(fileName)$size)
+      
     } else{
+      
       warning(paste("Failure in file", x[i]))
       txtfiles[i] <- ""
+      
     }
   }
   return(txtfiles)
