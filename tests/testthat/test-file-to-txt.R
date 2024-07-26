d23e966d55f0f5874ea05cdd2b52618b80cec389
MicheleNuijtene <michele.nuijten@gmail.com>
2024-07-26 10:36:24 +0200

    update html tests: don't hard code the "true" values, but extract them from the manual reference file

--- tests/testthat/test-file-to-txt.R
+++ tests/testthat/test-file-to-txt.R
@@ -77,57 +77,88 @@ test_that("stats from all pdfs in a folder are correctly retrieved & parsed", {
 test_that("statistics from a html are correctly retrieved and parsed", {
   
   html_file <- system.file("test_materials/nuijten.html",
-                          package = "statcheck")
+                           package = "statcheck")
   
   # skip test if file is not available
   if(html_file == "") skip("Test file not available.")
-
+  
+  # load the reference file with manually extracted statistics
+  manual <- load_manual(path_manual = "test_materials/manually_extracted_stats.csv", 
+                        file_id = "nuijten_html", apa = TRUE, typesetting_issues = FALSE)
+  
   result <- checkHTML(html_file, messages = FALSE)
   result_1tailed <- checkHTML(html_file, messages = FALSE, OneTailedTxt = TRUE)
   
   # extract 6 tests from paper
-  expect_equal(nrow(result), 6)
-  expect_equal(as.character(result[[VAR_TYPE]]), c("t", "Chi2", "t", "F", "F", "t"))
-  expect_equal(result[[VAR_TEST_VALUE]], c(-4.93, 6.9, 2, 1.203, 12.03, 2))
+  expect_equal(nrow(result), 
+               nrow(manual))
+  expect_equal(as.character(result[[VAR_TYPE]]), 
+               manual$test_type)
+  expect_equal(as.character(result[[VAR_TEST_VALUE]]), 
+               manual$test_value)
   
   # check errors
-  expect_equal(result[[VAR_ERROR]], c(FALSE, FALSE, FALSE, TRUE, FALSE, TRUE))
-  expect_equal(result[[VAR_DEC_ERROR]], c(FALSE, FALSE, FALSE, TRUE, FALSE, TRUE))
+  expect_equal(as.numeric(result[[VAR_ERROR]]), 
+               manual$error)
+  expect_equal(as.numeric(result[[VAR_DEC_ERROR]]), 
+               manual$decision_error)
   
   # check errors with one-tailed test detection
-  expect_equal(result_1tailed[[VAR_ERROR]], c(FALSE, FALSE, FALSE, TRUE, FALSE, FALSE))
-  expect_equal(result_1tailed[[VAR_DEC_ERROR]], c(FALSE, FALSE, FALSE, TRUE, FALSE, FALSE))
+  expect_equal(as.numeric(result_1tailed[[VAR_ERROR]]), 
+               manual$error_1tail)
+  expect_equal(as.numeric(result_1tailed[[VAR_DEC_ERROR]]), 
+               manual$decision_error_1tail)
   
 })
 
 # htmls in folder
-test_that("statistics from all htmls in a folder are correctly retrieved and parsed", {
+test_that("stats from all htmls in a folder are correctly retrieved & parsed", {
   
-  html_dir <- system.file("test_materials", package = "statcheck")
+  # this folder contains 4 "fake" pdf papers and 3 "fake" html/htm papers
+  # one of the pdf papers doesn't contain any stats
+  html_dir <- system.file("test_materials/test_dir", package = "statcheck")
   
   # skip test if files are not available
   if(!any(grepl(".htm*", list.files(html_dir)))) skip("Test files not available.")
   
+  # load the reference file with manually extracted statistics
+  manual <- 
+    load_manual(path_manual = "test_materials/test_dir/manually_extracted_stats_testpapers.csv",
+                apa = TRUE, file_type = "html")
+  
   result <- checkHTMLdir(html_dir, messages = FALSE, subdir = FALSE)
   
-  # extract 6+33 tests from papers 
-  expect_equal(nrow(result), 39)
+  # extract 11 tests from 3 papers
+  expect_equal(nrow(result), 
+               nrow(manual))
+  expect_equal(length(unique(result[[VAR_SOURCE]])), 
+               length(unique(manual$paper_id)))
   
 })
 
 # tests concerning parsing stats from all files ------------------------------
 
 # pdfs and htmls in folder
-test_that("statistics from all pdfs and htmls in a folder are correctly retrieved and parsed", {
-  
-  dir <- system.file("test_materials", package = "statcheck")
-  
-  # skip test if files are not available
-  if(!any(grepl(".htm*|.pdf", list.files(dir)))) skip("Test files not available.")
-  
-  result <- checkdir(dir, subdir = FALSE, messages = FALSE)
-  
-  # extract 92 tests (39 from html and 53 from pdf)
-  expect_equal(nrow(result), 92)
-})
+test_that("stats from all pdfs and htmls in a folder are correctly retrieved 
+          & parsed", {
+            
+            dir <- system.file("test_materials/test_dir", package = "statcheck")
+            
+            # skip test if files are not available
+            if(!any(grepl(".htm*|.pdf", list.files(dir)))) skip("Test files not available.")
+            
+            # load the reference file with manually extracted statistics
+            manual <- 
+              load_manual(path_manual = "test_materials/test_dir/manually_extracted_stats_testpapers.csv",
+                          apa = TRUE, file_type = "all")
+            
+            result <- checkdir(dir, subdir = FALSE, messages = FALSE)
+            
+            # extract 2*11 tests from 2*3 papers
+            # extract 11 tests from 3 papers
+            expect_equal(nrow(result), 
+                         nrow(manual))
+            expect_equal(length(unique(result[[VAR_SOURCE]])), 
+                         length(unique(manual$paper_id)))
+          })
 
