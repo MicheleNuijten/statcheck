6871cafa1adec2b8c2cf828868701f1952e351f2
MicheleNuijtene <michele.nuijten@gmail.com>
2024-07-26 09:40:07 +0200

    * don't test for other pdf converters; xpdf is removed. * realign test with regex: accept the letter l as df 1. * more stable programming; instead of hardcoding what to expect, retrieve manual result from .csv file with manually coded results

--- tests/testthat/test-file-to-txt.R
+++ tests/testthat/test-file-to-txt.R
@@ -3,35 +3,46 @@ context("Check if statistics from an article are correctly retrieved and parsed"
 # tests concerning parsing stats from pdf files ------------------------------
 
 # individual pdf files
-test_that("statistics from a pdf are correctly retrieved and parsed", {
+# pdftools
+test_that("pdftools correctly retrieves and parses statistics from a pdf", {
   
+  # load the pdf file of Nuijten et al. 2016
+  # https://doi.org/10.3758/s13428-015-0664-2
   pdf_file <- system.file("test_materials/nuijten.pdf",
                           package = "statcheck")
   
   # skip test if file is not available
   if(pdf_file == "") skip("Test file not available.")
   
-  result <- checkPDF(pdf_file, messages = FALSE)
-  result_1tailed <- checkPDF(pdf_file, messages = FALSE, OneTailedTxt = TRUE)
+  # load the reference file with manually extracted statistics
+  # load_manual is a helper function written for this package
+  manual <- load_manual(path_manual = "test_materials/manually_extracted_stats.csv", 
+                        file_id = "nuijten_pdf", apa = TRUE, typesetting_issues = FALSE)
   
-  # extract 4 tests from paper
-  expect_equal(nrow(result), 8)
-  expect_equal(as.character(result[[VAR_TYPE]]), c("t", "Chi2", "t", "F",
-                                                   "F", "F", "Chi2", "t"))
-  expect_equal(result[[VAR_TEST_VALUE]], c(-4.93, 6.9, 2, 1.203,
-                                           12.03, 23.95, 6.53, 2))
+  result <- checkPDF(pdf_file, messages = FALSE)
+  result_1tailed <- checkPDF(pdf_file, messages = FALSE,
+                             OneTailedTxt = TRUE)
+  
+  # extract 7 tests from paper
+  expect_equal(nrow(result), 
+               nrow(manual))
+  expect_equal(as.character(result[[VAR_TYPE]]), 
+               manual$test_type)
+  expect_equal(as.character(result[[VAR_TEST_VALUE]]), 
+               manual$test_value)
   
   # check errors
-  expect_equal(result[[VAR_ERROR]], c(FALSE, FALSE, FALSE, TRUE,
-                                      FALSE, FALSE, FALSE, TRUE))
-  expect_equal(result[[VAR_DEC_ERROR]], c(FALSE, FALSE, FALSE, TRUE,
-                                          FALSE, FALSE, FALSE, TRUE))
+  expect_equal(as.numeric(result[[VAR_ERROR]]), 
+               manual$error)
+  expect_equal(as.numeric(result[[VAR_DEC_ERROR]]), 
+               manual$decision_error)
   
   # check errors with one-tailed test detection
-  expect_equal(result_1tailed[[VAR_ERROR]], c(FALSE, FALSE, FALSE, TRUE,
-                                              FALSE, FALSE, FALSE, FALSE))
-  expect_equal(result_1tailed[[VAR_DEC_ERROR]], c(FALSE, FALSE, FALSE, TRUE,
-                                                  FALSE, FALSE, FALSE, FALSE))
+  expect_equal(as.numeric(result_1tailed[[VAR_ERROR]]), 
+               manual$error_1tail)
+  expect_equal(as.numeric(result_1tailed[[VAR_DEC_ERROR]]), 
+               manual$decision_error_1tail)
+  
 })
 
 # pdfs in folder
