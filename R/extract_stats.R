ac9217317bb820286a970080975056a0a2abf93f
Michèle Nuijten <michele.nuijten@gmail.com>
2026-03-10 14:54:17 +0100

    add apa_style argument throughout

--- R/extract_stats.R
+++ R/extract_stats.R
@@ -1,13 +1,20 @@
-extract_stats <- function(txt, stat){
+extract_stats <- function(txt, apa_style, stat){
   
   # step 1: extract all NHST results from text -----------------------------------
+  
+  # specify whether to search for APA NHST results or also to include non-APA
+  if(apa_style == TRUE){
+    rgx_nhst <- RGX_NHST
+  } else {
+    rgx_nhst <- RGX_NHST_NONAPA
+  }
   
   # if there are x NHST results in a text, this returns a character vector of 
   # length x
   nhst_raw <- extract_pattern(txt = txt,
                               # nhst is the regex for nhst results
                               # it came from the regex.R script within the package
-                              pattern = RGX_NHST,
+                              pattern = rgx_nhst,
                               # don't ignore case here: we only want to extract
                               # stats reported in a specific way (e.g., t, not T)
                               ignore.case = FALSE) 
@@ -17,7 +24,16 @@ extract_stats <- function(txt, stat){
     return(data.frame(NULL))
   }
   
-  # step 2: parse the extracted results ------------------------------------------
+  # step 2: clean non-apa results ----------------------------------------------
+  
+  # when non-apa results are extracted, it needs to be "translated" in the apa
+  # equivalent, to enable parsing of the result
+  # also "clean" apa results, to make sure apa and non-apa are parsed in the 
+  # same way eventually. For apa results, there shouldn't be many changes, other
+  # than that spaces are removed.
+  nhst_clean <- clean_non_apa(nhst_raw)
+  
+  # step 3: parse the extracted results ------------------------------------------
   
   # empty vectors and data frames to store results in
   # where relevant, force the output to be of type "character"
@@ -29,10 +45,10 @@ extract_stats <- function(txt, stat){
   # loop over all extracted, raw nhst results
   # each nhst result is parsed individually, in the order that they were extracted
   # from the text
-  for(i in seq_along(nhst_raw)){
+  for(i in seq_along(nhst_clean)){
     
     # extract the test types from the nhst results 
-    test_raw <- extract_pattern(txt = nhst_raw[i],
+    test_raw <- extract_pattern(txt = nhst_clean[i],
                                 pattern = RGX_TEST_TYPE)
     
     # classify the test types in standard classifications
@@ -67,7 +83,7 @@ extract_stats <- function(txt, stat){
       }
     } else {
       # if test_raw == NULL, chances are that the result is a chi2
-      # check if the nhst_raw matches the regex for chi2 and manually
+      # check if the nhst_clean matches the regex for chi2 and manually
       # add test type
       if(grepl(pattern = RGX_CHI2_DF, x = nhst_clean[i], 
                perl = TRUE)){
@@ -77,14 +93,15 @@ extract_stats <- function(txt, stat){
     
     # extract degrees of freedom
     
-    dfs <- extract_df(raw = nhst_raw[i],
+    dfs <- extract_df(raw = nhst_clean[i],
                       test_type = test_type[i])
     
     df_result <- rbind(df_result, dfs)
     
     # extract test comparison and test value 
     
-    test <- extract_test_stats(raw = nhst_raw[i])
+    test <- extract_test_stats(raw = nhst_clean[i],
+                               apa_style = apa_style)
     
     # ignore any cases with more than 1 test comparison (e.g., z >= 2)
     if(nrow(test) > 1){
@@ -97,7 +114,8 @@ extract_stats <- function(txt, stat){
     
     # extract p-comparison and p-value
     
-    p <- extract_p_value(raw = nhst_raw[i])
+    p <- extract_p_value(raw = nhst_clean[i], 
+                         apa_style = apa_style)
     
     # ignore any cases with more than 1 test comparison (e.g., p >= .01)
     if(nrow(p) > 1){
@@ -111,7 +129,7 @@ extract_stats <- function(txt, stat){
     
   }
   
-  # create final data frame ------------------------------------------------------
+  # step 4: create final data frame ------------------------------------------------------
   
   nhst_parsed <- data.frame(
     # return raw result without leading/trailing whitespaces
